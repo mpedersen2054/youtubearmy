cade026611596a92b837fe8041606132bc023bdf
Matt Pedersen <mpedersen2054@gmail.com>
2016-07-13 18:16:14 -0500

    add peices, which is comprised of the google/youtube login url & the dom selectors of the elements needed to login

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: true });
 var config = require('./config');
+
+var peices = {
+  loginUrl: 'https://accounts.google.com/ServiceLogin?continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Fapp%3Ddesktop%26action_handle_signin%3Dtrue%26next%3D%252F%26hl%3Den%26feature%3Dsign_in_button&passive=true&hl=en&service=youtube&uilel=3#identifier',
+  selectors: {
+    emailInp: '#Email',
+    emailBtn: '#next',
+    passwordInp: '#password-shown',
+    passwordBtn: '#signIn'
+  }
+}
